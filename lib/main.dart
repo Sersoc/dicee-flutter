456b2ec9275a0a6402b3f6fb01eac390d6821a58
Sersoc <45021157+Sersoc@users.noreply.github.com>
2024-10-09 20:04:19 +0900

    image Change

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue[50],
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.white,
         ),
         body: DicePage(),
       ),
@@ -18,6 +18,55 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        
+        Expanded(
+          flex: 3,
+          child: 
+          Image.asset('images/dice1.png'),
+            
+        ),
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            
+            
+          ),
+          ),
+        Expanded(
+          flex: 3,
+          child: Image.asset('images/dice4.png'),
+          
+          ),
+          
+          
+        // Text(
+        //   'JJ',
+        //   style: 
+        //   TextStyle(
+        //     fontSize: 20,
+        //     color: Colors.white,
+        //   ),
+        // ),
+        // Card(
+          
+        //   color: Colors.white,
+        //   child: Row(
+        //     children: <Widget>[
+        //       Text(
+                
+        //         'JJ',
+        //       style: TextStyle(
+        //         fontSize: 20,
+        //         color: Colors.black,
+
+        //       ),
+        //       ),
+        //     ],
+        //   ),
+        // )
+      ],
+    );
   }
 }
